226d4883b1d6fd5c39151c67fe25d2992ba04cab
jofanrizki <jofan_rizki_29rpl@student.smktelkom-mlg.co.id>
2022-04-29 07:02:31 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Navbar from "./components/navbar";
+import Main from "./main";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Main />
+      </div>
+    );
+  }
+}
